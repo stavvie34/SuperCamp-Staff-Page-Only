743524d3391d3f01ce4b736c863a0f21d7786141
stavvie34 <alexanderlee58col@gmail.com>
2017-12-28 12:41:28 -0800

    Google Structured Data for FAC position

---
+++ Sections/All/Google Jobs/Facilitator.js
@@ -0,0 +1,46 @@
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org",
+	"@type": "JobPosting",
+	"baseSalary" : {
+	  "@type": "MonetaryAmount",
+	  "currency": "USD",
+	  "value": {
+		  "@type": "QuantitativeValue",
+		  "value": 925.00,
+		  "unitText": "WEEK"
+	  }
+	}, 
+	"jobBenefits": "Room & Board, Travel",
+	"datePosted": "2017-12-27",
+	"description": "<p><strong>Are you a teacher or have you always wanted to work with youth? If so, take your passion for teaching to the next level - become a SuperCamp Facilitator. You&#39;ll have one of the most enriching professional development experiences of your career - and SuperCamp will transform the way you teach forever.</strong> This is a seasonal, salary position.</p><br><p>Since 1982, SuperCamp has become a worldwide leader in student programs that enhance learning, leadership, communication, and other essential skills for achievement in the 21st century. Our SuperCamp Facilitators are carefully chosen from hundreds of educators who are looking to inspire change. Only the most passionate and qualified make it to the final stage of hiring.<br></p><p>SuperCamp Facilitators bring their depth of knowledge of the curriculum, systems thinking, and leadership skills to create the culture of camp. Through their understanding of youth and their behavior, they are able to mentor both campers and staff.</p>",
+	"educationRequirements": "Bachelor's Degree",
+	"employmentType": "FULL_TIME",
+	"hiringOrganization" : {
+		"@type" : "Organization",
+		"name" : "SuperCamp",
+		"sameAs" : "http://www.supercamp.com/staff/",
+		"logo" : "https://www.supercamp.com/wp-content/uploads/2016/04/SuperCamp-50h-2.png"
+	},
+	"industry": "Education",
+	"jobLocation": {
+		"@type": "Place",
+		"address": {
+		  "@type": "PostalAddress",
+		  "streetAddress": "1938 Avenida Del Oro",
+		  "addressLocality": "Oceanside",
+		  "addressRegion": "CA",
+		  "postalCode": "92056",
+		  "addressCountry": "US"
+		}
+	},
+	"qualifications": "Experience working with youth. Ability to memorize and deliver large amounts of curriculum with accuracy. Attend an in-person facilitator tryout: Paving the Way - January 27th in Oceanside, CA (travel paid for by applicant).",
+	"responsibilities": "Facilitate Quantum Learning and life skills curriculum to a large group (up to 150 people) and lead large & small group discussions. ",
+	"salaryCurrency": "USD",
+	"title": "Facilitator",
+	"validThrough" : "2018-01-22",
+	"skills" : "Ability to memorize and deliver large amounts of curriculum with accuracy. Sound judgment, effective problem solving, and ability to build rapport with youth and peers. Vibrant and engaging front-of-the-room presence.",
+	"experienceRequirements" : "Some teaching experience preferred.",
+	"workHours" : "Varies"
+}
+</script>
