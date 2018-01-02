f7a038526fa6ee300a4630213cea05fe8d4ac8e9
stavvie34 <alexanderlee58col@gmail.com>
2018-01-02 10:01:43 -0800

    Added google structured data for TL job posting

---
+++ Sections/All/Google Jobs/Team Leader.js
@@ -0,0 +1,83 @@
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org",
+	"@type": "JobPosting",
+	"baseSalary" : {
+	  "@type": "MonetaryAmount",
+	  "currency": "USD",
+	  "value": {
+		"@type": "QuantitativeValue",
+		"minValue": 7.25,
+		"maxValue": 11.00,
+		"unitText": "HOUR"
+	  }
+	},
+	//"jobBenefits": "Room & Board, Travel",
+	"datePosted": "2017-12-29",
+	"description": "<p>Looking for a summer job that provides <strong>inspiration</strong>, <strong>professional development</strong>, and a chance to <strong>travel</strong>? If so, take your passion for leadership to the next level - become a SuperCamp Team Leader. You'll have one of the most enriching professional development experiences of your career - and SuperCamp will transform the way you lead forever. This is a seasonal, hourly position.</p><br><p>Since 1982, SuperCamp has become a worldwide leader in student programs that enhance learning, leadership, communication, and other essential skills for achievement in the 21st century. Our SuperCamp Facilitators are carefully chosen from hundreds of educators who are looking to inspire change. Only the most passionate and qualified make it to the final stage of hiring.<br></p><p>Being a SuperCamp Team Leader is an opportunity to have a lasting impact on the lives of others, and provides an opportunity for you to learn and grow as a leader. If you possess the qualifications, availability, and desire to work this summer, we encourage you to apply.</p>",
+	//"educationRequirements": "achelor's Degree",
+	"employmentType": "FULL_TIME",
+	"hiringOrganization" : {
+		"@type" : "Organization",
+		"name" : "SuperCamp",
+		"sameAs" : "http://www.supercamp.com/staff/",
+		"logo" : "https://www.supercamp.com/wp-content/uploads/2016/04/SuperCamp-50h-2.png"
+	},
+	"industry": "Education",
+	"jobLocation": 
+		[
+			{
+				"@type": "Place",
+				"address": {
+				  "@type": "PostalAddress",
+				  "streetAddress": "450 Serra Mall",
+				  "addressLocality": "Stanford",
+				  "addressRegion": "CA",
+				  "postalCode": "94305",
+				  "addressCountry": "US"
+				}
+			},
+			{
+				"@type": "Place",
+				"address": {
+				  "@type": "PostalAddress",
+				  "streetAddress": "4000 Central Florida Blvd",
+				  "addressLocality": "Orlando",
+				  "addressRegion": "FL",
+				  "postalCode": "32816",
+				  "addressCountry": "US"
+				}
+			},
+			{
+				"@type": "Place",
+				"address": {
+				  "@type": "PostalAddress",
+				  "streetAddress": "1001 E University Ave",
+				  "addressLocality": "Georgetown",
+				  "addressRegion": "TX",
+				  "postalCode": "78626",
+				  "addressCountry": "US"
+				}
+			},
+			{
+				"@type": "Place",
+				"address": {
+				  "@type": "PostalAddress",
+				  "streetAddress": "800 Lancaster Ave",
+				  "addressLocality": "Villanova",
+				  "addressRegion": "PA",
+				  "postalCode": "19085",
+				  "addressCountry": "US"
+				}
+			}
+		],
+	"qualifications": "At least 20 years old (by September 1st, 2018) with at least one completed academic year at a college or university. Excited to work with youth in an indoor/outdoor setting and live in a close-knit community. Able to lift at least 20 lbs.",
+	"responsibilities": "Co-lead a team of 10-15 campers (ages 11-18) with another Team Leader. Lead engaging and thought provoking conversations to deepen the camper experience. Commit to do whatever it takes to create a safe, supportive, and joyful learning environment. Lead activities such as soccer, basketball, yoga, dance, or anything else you can come up with!",
+	"salaryCurrency": "USD",
+	"title": "Team Leader",
+	"validThrough" : "2018-08-01",
+	//"skills" : "Ability to memorize and deliver large amounts of curriculum with accuracy. Sound judgment, effective problem solving, and ability to build rapport with youth and peers. Vibrant and engaging front-of-the-room presence.",
+	"experienceRequirements" : "Experience in working with youth (Preferred)",
+	"workHours" : "Varies"
+}
+</script>
