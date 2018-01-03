433e0b09bef11ca27a5c570b979d799e2ca39eda
stavvie34 <alexanderlee58col@gmail.com>
2018-01-03 15:45:37 -0800

    Try to move script from consolidated into the other script on server

--- Sections/All/Countdowns/TimeCircles.js
+++ Sections/All/Countdowns/TimeCircles.js
@@ -946,3 +946,275 @@
         return new TC_Class(this, options);
     };
 }(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+//FROM COUNTDOWNS
+$("#DateCountdown, #DateCountdown2, #DateCountdown3").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": true
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": true
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": true
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": true
+		}
+	}
+});
+
+$("#DateCountdownDayHour2").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": true
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": true
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": false
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": false
+		}
+	}
+});
+
+$("#DateCountdownMinuteSecond2").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": false
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": false
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": true
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": true
+		}
+	}
+});
+
+$("#DateCountdownDayHour3").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": true
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": true
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": false
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": false
+		}
+	}
+});
+
+$("#DateCountdownMinuteSecond3").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": false
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": false
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": true
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": true
+		}
+	}
+});
+
+$("#DateCountdownDayHour4").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": true
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": true
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": false
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": false
+		}
+	}
+});
+
+$("#DateCountdownMinuteSecond4").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": false
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": false
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": true
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": true
+		}
+	}
+});
+
+$("#DateCountdownDayHour5").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": true
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": true
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": false
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": false
+		}
+	}
+});
+
+$("#DateCountdownMinuteSecond5").TimeCircles({
+	"animation": "smooth",
+	"bg_width": 0.6,
+	"fg_width": 0.06666666666666667,
+	"circle_bg_color": "#60686F",
+	"time": {
+		"Days": {
+			"text": "Days",
+			"color": "#FFCC66",
+			"show": false
+		},
+		"Hours": {
+			"text": "Hours",
+			"color": "#99CCFF",
+			"show": false
+		},
+		"Minutes": {
+			"text": "Minutes",
+			"color": "#BBFFBB",
+			"show": true
+		},
+		"Seconds": {
+			"text": "Seconds",
+			"color": "#FF9999",
+			"show": true
+		}
+	}
+});
